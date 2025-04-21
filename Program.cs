c0bf4a1b2034d9994b5542382d069424bb20549a
songdh001 <155062686+songdh001@users.noreply.github.com>
2025-04-21 17:02:19 +0900

    Update Program.cs
    실행되는지 테스트 + Console.WriteLine("테스트입니다."); 한 줄 추가

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TextRPG_8Team;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("테스트입니다.");
+    }
+}
