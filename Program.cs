bb0c5cc96c980d1f6f5feffb3976026b1eec5b61
SongAri <155062686+songdh001@users.noreply.github.com>
2025-04-21 19:25:16 +0900

    Revert "스테이터스 파일"

--- Program.cs
+++ Program.cs
@@ -3,12 +3,10 @@
 class Program
 {
 
-    static Player player = new Player();
-
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         Console.WriteLine("테스트입니다.");
-        Console.WriteLine("Branch 테스트");        
+        Console.WriteLine("Branch 테스트");
     }
 }
