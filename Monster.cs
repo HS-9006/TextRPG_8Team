f1c9c44a5e16157e614b81cc2edd709c3b86a62f
YJR-CAMPER <eganom2806@gmail.com>
2025-04-21 19:49:16 +0900

    몬스터 추가
    제곧내

---
+++ Monster.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace TextRPG_8Team
+{
+    public class Monster
+    {
+        public string name;
+        public int level;
+        public int health { get; set; }
+        public int attack { get; set; }
+        public int def { get; set; }
+        public int speed { get; set; }
+        public int exp { get; set; }
+        public int gold { get; set; }
+        public bool isAlive => health > 0;
+    }
+
+    public class Ssalsoong : Monster
+    {
+        public Ssalsoong()
+        {
+            name = "쌀숭이";
+            level = 1;
+            health = 30;
+            attack = 10;
+            def = 5;
+            speed = 15;
+            exp = 10;
+            gold = 5;
+        }
+    }
+
+    public class Reshoongjwak : Monster
+    {
+        public Reshoongjwak()
+        {
+            name = "리슝좍";
+            level = 2;
+            health = 35;
+            attack = 15;
+            def = 7;
+            speed = 20;
+            exp = 15;
+            gold = 10;
+        }
+    }
+}
