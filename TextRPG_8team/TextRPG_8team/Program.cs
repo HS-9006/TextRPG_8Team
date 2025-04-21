d595ef415d54f28a04ac015e4cad21f134f04992
ckdals652 <ckdals652@naver.com>
2025-04-21 17:10:47 +0900

    - Init - setting
    - Init - setting

---
+++ TextRPG_8team/TextRPG_8team/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TextRPG_8team
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
