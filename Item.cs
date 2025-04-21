2a64cd13b6446e7f6149612c6055e021154bb6c8
songdh001 <155062686+songdh001@users.noreply.github.com>
2025-04-21 19:23:05 +0900

    스테이터스 파일
    스테이터스 파일

---
+++ Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_8Team
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int AttackBonus { get; set; }
+        public int DefenseBonus { get; set; }
+        public int Price { get; set; }
+
+        public Item() { }
+
+        public Item(string name, int atk, int def, int price)
+        {
+            Name = name;
+            AttackBonus = atk;
+            DefenseBonus = def;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} (공격력 +{AttackBonus}, 방어력 +{DefenseBonus}, 가격: {Price}G)";
+        }
+
+    }
+}
