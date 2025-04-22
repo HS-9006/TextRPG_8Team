b9f84a88b068605e13256d5b7cb94c02ee72c456
YJR-CAMPER <eganom2806@gmail.com>
2025-04-22 10:29:32 +0900

    불필요한 부분 삭제(추후 필요시 복구)

--- Monster.cs
+++ Monster.cs
@@ -11,36 +11,8 @@ namespace TextRPG_8Team
         public int speed { get; set; }
         public int exp { get; set; }
         public int gold { get; set; }
-        public int amount { get; set; }
         public bool isAlive => health > 0;
 
-        public virtual void Attack(Monster target)
-        {
-            if (!isAlive)
-            {
-                Console.WriteLine($"{name}은(는) 사망 상태라 공격할 수 없습니다.");
-                return;
-            }
-
-            if (!target.isAlive)
-            {
-                Console.WriteLine($"{target.name}은(는) 이미 사망했습니다. 공격 무효!");
-                return;
-            }
-
-            int damage = attack - target.def;
-            damage = Math.Max(0, damage);
-            target.health -= damage;
-
-            Console.WriteLine($"{name}이(가) {target.name}에게 {damage}의 피해를 입혔습니다!");
-
-            if(target.health < 0)
-            {
-                target.health = 0;
-                target.isAlive = false;
-                Console.WriteLine($"{target.name}은(는) 쓰러졌습니다.");
-            }
-        }
 
     }
 }
